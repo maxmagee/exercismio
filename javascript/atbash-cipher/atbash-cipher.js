350145d3cf60607e1b07ebaa972efc4d37d988d7
Max Magee <maxmagee@users.noreply.github.com>
2018-02-23 22:23:22 -0600

    Atbash Cipher: Refactored the regular expressions down to one simple expression.

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -1,8 +1,5 @@
 const minAsciiCode = 97;    // a
 const maxAsciiCode = 122;   // z
-const commaPattern = /,/g;
-const punctuationPattern = /[\u00A1\u00BF\u2000-\u206F\u2E00-\u2E7F\\!"#$%&()*+\-.\/:;<=>?@\[\]^_`{|}~]/g;
-const spacePattern = /\s/g;
 
 // Given a returns z. Given b returns y. Requires letter-case characters.
 const reverseAlphabet = (inputChar) => {
@@ -17,9 +14,7 @@ const atbash = {
     encode: (input) => {
         return input
             .toLowerCase()
-            .replace(commaPattern, '')
-            .replace(punctuationPattern, '')
-            .replace(spacePattern, '')
+            .replace(/[^a-z0-9]/g,'')   // Remove non alphanumeric lowercase characters
             .split('')
             .map(reverseAlphabet)
             .join('')
